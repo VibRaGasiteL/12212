0562157f3fb8ef5c5ea590ad2251712d2dee007d
VibRaGasiteL <nikitka4321@gmail.com>
2016-02-12 23:02:15 +0300

    121212
    213321312323

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(A(7));
+            Console.ReadKey();
+
+        }
+        static int A(int i)
+        {
+            int j = 1;
+            while (i > j)
+                j += i--;
+            return j;
+        }
+    }
+}
